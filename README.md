a9ef197973767b8c6eaf73dc5efa97def5877caa
George Chung <Gerhut@GMail.com>
2016-03-16 23:56:54 +0800

    initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # koa-inherit-state
+
+[![Build Status](https://travis-ci.org/Gerhut/koa-inherit-state.svg?branch=master)](https://travis-ci.org/Gerhut/koa-inherit-state)
+[![codecov.io](https://codecov.io/github/Gerhut/koa-inherit-state/coverage.svg?branch=master)](https://codecov.io/github/Gerhut/koa-inherit-state?branch=master)
+[![Dependency Status](https://david-dm.org/gerhut/koa-inherit-state.svg)](https://david-dm.org/gerhut/koa-inherit-state)
+[![devDependency Status](https://david-dm.org/gerhut/koa-inherit-state/dev-status.svg)](https://david-dm.org/gerhut/koa-inherit-state#info=devDependencies)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
 Make context.state inherits app.context.state
+
+## Install
+
+    $ npm install --save koa-inherit-state
+
+## Usage
+
+TODO, See test case.
+
+## License
+
+MIT
---
+++ test/index.js
@@ -0,0 +1,41 @@
+/* eslint-env mocha */
+'use strict'
+
+const should = require('should')
+const koa = require('koa')
+const request = require('supertest')
+
+const inheritState = require('..')
+
+describe('koa-inherit-state', function () {
+  it('this.state in middleware will inherit app.context.state', function (done) {
+    const app = koa()
+    app.context.state = { 'foo': 'bar' }
+    app.use(inheritState())
+    app.use(function * () {
+      should(this.state.foo).be.equal('bar')
+      this.status = 204
+    })
+
+    request(app.listen()).get('/')
+      .expect(204).end(done)
+  })
+
+  it('different context.state will not affect others', function (done) {
+    const app = koa()
+    app.context.state = { 'foo': 'bar' }
+    app.use(inheritState())
+    app.use(function * () {
+      should(this.state.foo).be.equal('bar')
+      this.status = 204
+    })
+
+    const server = app.listen()
+    request(server).get('/')
+      .expect(204).end((err) => {
+        if (err) return done(err)
+        request(server).get('/')
+          .expect(204).end(done)
+      })
+  })
+})
